63591937b9516c0700795e1183169282c14d1d12
Morel-D <morelDen@gmail.com>
2023-08-12 01:47:43 +0100

    Wrap the index with provider containing store as parameter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import blogReducer from './components/redux/reducers/blogReducer';
+
+const store = createStore(blogReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+     <App />
+    </Provider>
   </React.StrictMode>
 );
