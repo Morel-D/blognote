29db7c209c393838ccd2441a22e87c5e41314c24
Morel-D <morelDen@gmail.com>
2023-08-22 21:24:45 +0100

    app setup

---
+++ server/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// Express App
+const app = express();
+
+
+// Middleware 
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+app.use(cors());
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening to port', process.env.PORT)
+});
