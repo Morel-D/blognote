75599351a8010a80a6cf616ff820a7b23c9df26b
Morel-D <morelDen@gmail.com>
2023-08-23 20:29:53 +0100

    controller  setup 2

--- server/controller/blogController.js
+++ server/controller/blogController.js
@@ -1,27 +1,69 @@
+const Blog = require('../model/blogModel');
+const mongoose = require('mongoose');
 
+
+// Add the records to the database
 const addBlog = (req, res) => 
 {
-    res.json({message : "The data has been added"});
+    const newBlog = new Blog(req.body);
+    newBlog.save();
+
+    Blog.find().then((results) => {
+        res.json(results);
+    }).catch((error) => {
+        res.json(error.message)
+    })
 }
+
+// Display all the records
 
 const getBlog  = (req, res) =>
 {
-    res.json({message : "The data has been seen"});
+    Blog.find().sort({ createdAt: -1 })
+    .then((results) => {
+        res.json(results)
+    }).catch((error) => {
+        res.json(error.message)
+    });
 }
 
 const getSingleBlog = (req, res) => 
 {
-    res.json({message: "Single blog has been added"});
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    Blog.findById(id)
+    .then((results) => {
+        res.json(results)
+    }).catch((error) => {
+        res.json(error)
+    })
 }
 
 const deleteBlog = (req, res) => 
 {
-    res.json({message: "The blog has been deleted"});
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    Blog.findByIdAndRemove(id).then((results) => {
+        res.json(results)
+    }).catch((error) => {
+        res.json(error.message)
+    })
 }
 
 const updateBlog = (req, res) => 
 {
-    res.json({message: "The blog has been updated"})
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    
+    const newBlog = 
+    {
+        title: req.body.title,
+        content: req.body.content
+    }
+
+    Blog.findByIdAndUpdate(id, newBlog)
+    .then((results) => {
+        res.json(results)
+    }).catch((error) => {
+        res.json(error.message)
+    })
 }
 
 module.exports = 
