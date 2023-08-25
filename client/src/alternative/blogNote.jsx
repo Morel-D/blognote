62f4d32cb10e174d514ce53a4f7110ef8214e0d6
Morel-D <morelDen@gmail.com>
2023-08-25 01:44:49 +0100

    Initialize Axios for React Hooks Redux CRUD HTTP Client

--- client/src/alternative/blogNote.jsx
+++ client/src/alternative/blogNote.jsx
@@ -1,48 +1,48 @@
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, reset } from "./redux/actions/blogAction";
-import { useEffect } from "react";
-import { fetchData } from "./redux/actions/requestAction";
+// import { useDispatch, useSelector } from "react-redux";
+// import { decrement, increment, reset } from "./redux/actions/blogAction";
+// import { useEffect } from "react";
+// import { fetchData } from "./redux/actions/requestAction";
 
-const BlogNoate = () => {
+// const BlogNoate = () => {
 
-    const blog = useSelector(state => state.blog);
-    const blogRequest = useSelector(state => state.blogRequest.data);
-    const dispatch = useDispatch();
+//     const blog = useSelector(state => state.blog);
+//     const blogRequest = useSelector(state => state.blogRequest.data);
+//     const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchData());
-    }, [blogRequest]);
+//     useEffect(() => {
+//         dispatch(fetchData());
+//     }, [blogRequest]);
 
-    console.log("The Data is: ", blogRequest.title);
+//     console.log("The Data is: ", blogRequest.title);
 
 
-    return ( 
-        <div className="blog-note">
-            <div className="container d-flex justify-content-center mt-5">
-                <div className="container mt-5 d-flex justify-content-center text-center  d-flex gap-3">
-                    <div className="card col-5 p-5">
-                        <h2 className="text-secondary">Number of blogs - {blog}</h2>
-                        <br />
-                        <div className="row">
-                            <div className="col">
-                                <button className="btn btn-warning" onClick={() => dispatch(increment())}>Add Note</button>
-                            </div>
-                            <div className="col">
-                                <button className="btn btn-dark" onClick={() => dispatch(reset())}>Reset</button>
-                            </div>
-                            <div className="col">
-                               <button className="btn btn-danger" onClick={ () => dispatch(decrement())}>Delete Note</button>
-                            </div>
-                        </div>
-                    </div>
-                    <br />
-                    <div className="card col-5 p-5">
-                        {blogRequest.title}
-                    </div>
-                </div>
-            </div>
-        </div>
-     );
-}
+//     return ( 
+//         <div className="blog-note">
+//             <div className="container d-flex justify-content-center mt-5">
+//                 <div className="container mt-5 d-flex justify-content-center text-center  d-flex gap-3">
+//                     <div className="card col-5 p-5">
+//                         <h2 className="text-secondary">Number of blogs - {blog}</h2>
+//                         <br />
+//                         <div className="row">
+//                             <div className="col">
+//                                 <button className="btn btn-warning" onClick={() => dispatch(increment())}>Add Note</button>
+//                             </div>
+//                             <div className="col">
+//                                 <button className="btn btn-dark" onClick={() => dispatch(reset())}>Reset</button>
+//                             </div>
+//                             <div className="col">
+//                                <button className="btn btn-danger" onClick={ () => dispatch(decrement())}>Delete Note</button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <br />
+//                     <div className="card col-5 p-5">
+//                         {blogRequest.title}
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//      );
+// }
  
-export default BlogNoate;
+// export default BlogNoate;
--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,16 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import myReducers from './components/redux/reducers/combineReducers';
 import thunk from 'redux-thunk';
 
 // const applyMiddleware = redux.applyMiddleware;
-const store = createStore(myReducers, applyMiddleware(thunk));
+// const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
      <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
